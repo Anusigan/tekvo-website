1ad594b16d658afdf57e099d2303a834a7b905fa
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2025-05-26 12:08:42 +0530

    updated supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -5,7 +5,7 @@ export const createBrowserClient = () => {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
   const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 
-  return createClient(supabaseUrl, supabaseAnonKey)
+  return createClient(supabaseUrl,supabaseAnonKey)
 }
 
 // Create a server client
